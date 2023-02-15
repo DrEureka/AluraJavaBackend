e4aeaf6702467e4bc18e675c0946d299e8c97216
Dr Eureka <drspam@duck.com>
2023-02-15 12:03:24 -0300

    Alura practicas en java

---
+++ src/TipoVariable.java
@@ -0,0 +1,20 @@
+public class TipoVariable {
+    public static void main(String[] args){
+        System.out.println("Tipos de variables: ");
+        System.out.println(" ");
+        System.out.println("Tipo int: ");
+        int edad = 23;
+        System.out.println(edad);
+        System.out.println("Como tengo declarada la variable 'edad' puedo asignar otros valores a la variable sin indicar el tipo de datos: ");
+        edad = 190;
+        System.out.println(edad);
+        System.out.println("Suma de valores 190 + 150, utilizando la variable edad: ");
+        edad = 190 + 150;
+        System.out.println("La suma de los valores es: " + edad);
+        System.out.println(" ");
+        System.out.println("Tipo double: ");
+        double decimal = 15.0;
+        System.out.println(decimal);
+
+    }
+}
---
+++ src/ejemplo.java
@@ -0,0 +1,7 @@
+public class ejemplo {
+    public static void main(String[] args) {
+        System.out.println("Esto es un ejemplo de print en java!");
+        System.out.println("Esto es otra linea de codigo");
+        System.out.println("Test 123");
+    }
+}
