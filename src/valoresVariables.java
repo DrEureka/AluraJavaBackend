b44c85c98d2c071ef07106f61089ca51e7e2fc22
Dr Eureka <drspam@duck.com>
2023-02-15 16:05:51 -0300

    Avances en practicas en java JRE y JDK

--- src/valoresVariables.java
+++ src/valoresVariables.java
@@ -1,17 +1,14 @@
 public class valoresVariables {
     public static void main(String[] args) {
        System.out.println("Condicionales if: ");
+        System.out.println("El or en java es ||");
 
        int edad = 17;
-       int cantidad = 2;
-       if (edad >= 18) {
+       int cantidadPersonas = 2;
+       if (edad >= 18 || cantidadPersonas <= 2) {
            System.out.println("Es mayor de 18, puede ingresar.");
        }
        else {
-           if (cantidad >=2 ) {
-               System.out.println("Es menor de edad pero puede ingresar al ser 2 personas.");
-           }else
-
            System.out.println("No puede ingresar, no es mayor de 18.");
        }
     }
